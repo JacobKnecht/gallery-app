6515b51cc3754f18e1f308e236be4ac31c0cfd4a
Jacob Knecht <jacobknecht3@gmail.com>
2019-06-14 13:45:51 -0500

    initialized GalleryItem.js as a class component

---
+++ src/components/GalleryItem.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class GalleryItem extends Component {
+  //Purpose: reusable (with iteration) to display each list item and image
+  //Should be stateless component
+  //Should receive props from App component
+  render() {
+    return (
+      
+    );
+  }
+}
+
+export default GalleryItem;
