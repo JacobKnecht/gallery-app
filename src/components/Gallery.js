73cfbd8674591cddbc97e9fc65d59a2f899f6a42
Jacob Knecht <jacobknecht3@gmail.com>
2019-06-14 13:45:21 -0500

    initialized Gallery.js as a class component

---
+++ src/components/Gallery.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Gallery extends Component {
+  //Purpose: reusable display for the sets of images for each topic category
+  //Should be stateless component
+  //Should receive props from App component
+  render() {
+    return (
+      
+    );
+  }
+}
+
+export default Gallery;
