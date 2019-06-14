867fe475280d49573db623a568199df0c0d77dda
Jacob Knecht <jacobknecht3@gmail.com>
2019-06-14 13:46:30 -0500

    initialized Nav.js as a class component

---
+++ src/components/Nav.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Nav extends Component {
+  //Purpose: render navigation menu
+  //Should be stateless component
+  //Should receive props from App component
+  render() {
+    return (
+      
+    );
+  }
+}
+
+export default Nav;
