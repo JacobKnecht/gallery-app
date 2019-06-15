5fb9d1f431869f7e9eb583e4681855b42adcf0ff
Jacob Knecht <jacobknecht3@gmail.com>
2019-06-15 12:55:05 -0500

    added React Router import statement

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Header from './components/Header'
 
 function App() {
   return (
-    <div className="App">
-      <Header message={"REACT IMAGE GALLERY APP"}/>
+    <div className="container">
+      <Header message={"REACT IMAGE GALLERY APP"} />
     </div>
   );
 }
