fd28aa27bb732df2f81cfeb226382b69d7b9409a
Jacob Knecht <jacobknecht3@gmail.com>
2019-06-15 12:56:36 -0500

    added form JSX, local state, and handlers for form submission and input change

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -3,9 +3,34 @@ import React, { Component } from 'react';
 class SearchForm extends Component {
   //Purpose: to display the search input and button
   //Should contain its own local state
+  state = {
+    searchText: '';
+  }
+
+  //sets the search input text state to the text currently in the input
+  onSearchChange = event => {
+    this.setState({ searchText: event.target.value });
+  }
+
+  //resets search input on submission
+  handleSubmit = event => {
+    e.preventDefault;
+    e.currentTarget.reset();
+  }
+
   render() {
     return (
-      console.log('SearchForm exists');
+      <form  className="search-form" onSubmit={this.handleSubmit}>
+        <input
+          type="search"
+          name="search"
+          onChange={this.onSearchChange}
+          placeholder="Search"
+        />
+        <button type="submit">
+          Jake
+        </button>
+      </form>
     );
   }
 }
