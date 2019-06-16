1b25ed4f154990cc3229020985f88e3e3ed17334
Jacob Knecht <jacobknecht3@gmail.com>
2019-06-16 00:01:45 -0500

    created Gallery component JSX

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import GalleryItem from './GalleryItem';
 
 class Gallery extends Component {
   //Purpose: reusable display for the sets of images for each topic category
@@ -6,7 +7,12 @@ class Gallery extends Component {
   //Should receive props from App component
   render() {
     return (
-      
+      <div className="photo-container">
+        <h2>Results</h2>
+        <ul>
+          <GalleryItem images={this.props.images}/>
+        </ul>
+      </div>
     );
   }
 }
