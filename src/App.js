b315d2db0c45ff28c532eb907a1270fb276dff80
Jacob Knecht <jacobknecht3@gmail.com>
2019-06-20 22:22:56 -0500

    created componentDidMount method to fetch data from Flickr API

--- src/App.js
+++ src/App.js
@@ -5,16 +5,17 @@ import Gallery from './components/Gallery';
 import apiKey from './config.js';
 
 
-//data needs to be fetched here
-
 class App extends Component {
   state = {
-    images : [
-      <img src="https://farm5.staticflickr.com/4334/37032996241_4c16a9b530.jpg" alt="" />,
-      <img src="https://farm5.staticflickr.com/4342/36338751244_316b6ee54b.jpg" alt="" />,
-      <img src="https://farm5.staticflickr.com/4343/37175099045_0d3a249629.jpg" alt="" />,
-      <img src="https://farm5.staticflickr.com/4425/36337012384_ba3365621e.jpg" alt="" />
-    ],
+    images : [],
+  }
+
+  componentDidMount() {
+    //data needs to be fetched here
+    fetch(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=walrus&extras=url_o&per_page=24&format=json&nojsoncallback=1`)
+      .then(response => response.json())
+      .then(responseData => this.setState({ images: responseData.photos.photo }))
+      .catch(error => console.log('There was an error fetching the data...'));
   }
 
   render() {
