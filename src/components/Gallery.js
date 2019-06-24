ade4ee15a7b05dc70fb3ad65d870d966ce90e554
Jacob Knecht <jacobknecht3@gmail.com>
2019-06-24 16:45:30 -0500

    added NoItems import and return conditional in the Gallery component's render method

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,29 +1,37 @@
 import React, { Component } from 'react';
 import GalleryItem from './GalleryItem';
+import NoItems from './NoItems';
 
 class Gallery extends Component {
   //Purpose: reusable display for the sets of images for each topic category
   //Should be stateless component
   //Should receive props from App component
   render() {
-    return (
-      <div className="photo-container">
-        <h2>{this.props.title}</h2>
-        <ul>
-          {
-            this.props.images.map(image => {
-              return (
-                <GalleryItem
-                  key={image.id}
-                  source={`https://farm${image.farm}.staticflickr.com/${image.server}/${image.id}_${image.secret}.jpg`}
-                  alt={image.title} 
-                />
-              )
-            })
-          }
-        </ul>
-      </div>
-    );
+    if(this.props.images.length > 0) {
+      return (
+          <div className="photo-container">
+            <h2>{this.props.title}</h2>
+            <ul>
+              {
+                this.props.images.map(image => {
+                  return (
+                    <GalleryItem
+                      key={image.id}
+                      source={`https://farm${image.farm}.staticflickr.com/${image.server}/${image.id}_${image.secret}.jpg`}
+                      alt={image.title}
+                    />
+                  )
+                })
+              }
+            </ul>
+          </div>
+      );
+    } else {
+      return (
+        <NoItems title={this.props.title} />
+      );
+    }
+
   }
 }
 
